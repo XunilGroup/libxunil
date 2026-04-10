9047b85e3221364d15fe2320b620407d5d327172
csd4ni3l <v3xq21w7@duck.com>
2026-04-10 12:50:35 +0200

    Fix left-in write stub instead of the correct one.

--- src/lib.rs
+++ src/lib.rs
@@ -39,7 +39,7 @@ static TOUPPER_TABLE: [i32; 384] = {
 
 #[unsafe(no_mangle)]
 extern "C" fn write(fd: i32, buf: *const u8, count: usize) -> isize {
-    0 // unsafe { syscall3(WRITE, fd as isize, buf as isize, count as isize) }
+    unsafe { syscall3(WRITE, fd as isize, buf as isize, count as isize) }
 }
 
 #[unsafe(no_mangle)]
